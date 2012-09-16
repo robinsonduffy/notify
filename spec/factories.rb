6763d50b12506c392bc0cf42af35a530b813a037
Robinson Duffy <robinson.duffy@gmail.com>
2012-09-16 08:58:25 -0800

    Recipient Factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,3 +3,10 @@ Factory.define :user do |user|
   user.password "foobar"
   user.name "Test User"
 end
+
+Factory.define :recipient do |recipient|
+  recipient.external_id "12345"
+  recipient.recipient_type "student"
+  recipient.first_name "Test"
+  recipient.last_name "Student"
+end
