74e40a5fcf28097be3b0aebc23a672760cfcb7e7
Robinson Duffy <robinson.duffy@gmail.com>
2012-10-10 12:52:02 -0800

    MessagePermissions for RecipientType

--- app/models/recipient_type.rb
+++ app/models/recipient_type.rb
@@ -1,5 +1,6 @@
 class RecipientType < ActiveRecord::Base
-  has_many :recipients
+  has_many :recipients, :dependent => :destroy
+  has_many :message_permissions, :as => :object, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
 end
